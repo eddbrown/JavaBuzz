52b99cc6c1ebdc6c569019a52baf7319bccf0f09
Edward Brown <edward.brown.london@gmail.com>
2015-03-03 12:26:09 +0000

    return Java for 3, Buzz for 5, JavaBuzz for 15

---
+++ spec/JavabuzzSpec.js
@@ -0,0 +1,55 @@
+describe('Javabuzz', function() {
+  var javabuzz;
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
+  describe('knows when a number is', function() {
+
+    it('divisible by 3', function(){
+      expect(javabuzz._isDivisibleBy(3, 3)).toBe(true);
+    });
+
+    it('divisible by 5', function(){
+      expect(javabuzz._isDivisibleBy(5, 5)).toBe(true);
+    });
+
+    it('divisible by 5 and 3', function(){
+      expect(javabuzz._isDivisibleBy(15, 15)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisible by 3', function(){
+      expect(javabuzz._isDivisibleBy(1, 3)).toBe(false);
+    });
+
+    it('divisible by 5', function(){
+      expect(javabuzz._isDivisibleBy(2, 5)).toBe(false)
+    });
+
+    it('divisible by 15', function(){
+      expect(javabuzz._isDivisibleBy(4, 15)).toBe(false)
+    });
+  });
+
+  describe('when playing, says', function() {
+
+    it('"Java" when a number is divisible by 3', function(){
+      expect(javabuzz.says(3)).toEqual("Java");
+    })
+
+    it('"Java" when a number is divisible by 5', function(){
+      expect(javabuzz.says(5)).toEqual("Buzz");
+    })
+
+    it('"Java" when a number is divisible by 3 and 5', function(){
+      expect(javabuzz.says(15)).toEqual("Javabuzz");
+    })
+
+    it('the number, if not divisible by 3, 5 or 15', function(){
+      expect(javabuzz.says(2)).toEqual(2);
+    });
+  });
+});
---
+++ src/Javabuzz.js
@@ -0,0 +1,12 @@
+var Javabuzz = function () {};
+
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
+};
+
+Javabuzz.prototype.says = function(number) {
+  if(this._isDivisibleBy(number, 15)) return 'Javabuzz';
+  if(this._isDivisibleBy(number, 3)) return 'Java';
+  if(this._isDivisibleBy(number, 5)) return 'Buzz';
+  return number;
+};
